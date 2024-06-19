c8584ccd7ef0a01ab5c2b0a43d7c899d94a6bfd8
Jair Quinto <tokinmym2@gmail.com>
2024-06-19 09:19:21 -0500

    Api - Filter

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,15 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/tasks', 'App\Http\Controllers\Api\TaskController@index');
+    Route::post('/tasks', 'App\Http\Controllers\Api\TaskController@store');
+    Route::get('/tasks/{task}', 'App\Http\Controllers\Api\TaskController@show');
+    Route::put('/tasks/{task}', 'App\Http\Controllers\Api\TaskController@update');
+    Route::delete('/tasks/{task}', 'App\Http\Controllers\Api\TaskController@destroy');
+    Route::get('/tasks/{task}/edit', 'App\Http\Controllers\Api\TaskController@edit');
+    Route::get('/tasks/filter', 'App\Http\Controllers\Api\TaskController@filter');
+
 });
-
-
-Route::get('/tasks', 'App\Http\Controllers\Api\TaskController@index');
-Route::post('/tasks', 'App\Http\Controllers\Api\TaskController@store');
-Route::get('/tasks/{task}', 'App\Http\Controllers\Api\TaskController@show');
-Route::put('/tasks/{task}', 'App\Http\Controllers\Api\TaskController@update');
-Route::delete('/tasks/{task}', 'App\Http\Controllers\Api\TaskController@destroy');
-Route::get('/tasks/{task}/edit', 'App\Http\Controllers\Api\TaskController@edit');
-Route::get('/tasks/create', 'App\Http\Controllers\Api\TaskController@create');
